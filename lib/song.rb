d004ab1b9bfa28a455a36ca6dce4c22b73d6f207
Michael Rabon <nobar86@gmail.com>
2020-08-06 20:30:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,12 @@ class Song
   @@genres = []
   @@artists = []
 
-  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
 end
